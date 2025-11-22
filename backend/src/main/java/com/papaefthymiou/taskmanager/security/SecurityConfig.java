f2ad4f29049ad45ed57a0078bd3440848a647ca4
Neurot <em.papaefthimiou@gmail.com>
2025-11-23 00:17:14 +0200

    add global CORS configuration to allow Angular frontend access, creating service for frontend

--- backend/src/main/java/com/papaefthymiou/taskmanager/security/SecurityConfig.java
+++ backend/src/main/java/com/papaefthymiou/taskmanager/security/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfig {
@@ -45,5 +47,19 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults());       // Allow CORS
 
         return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
